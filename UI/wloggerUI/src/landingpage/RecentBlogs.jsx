4e3678255ce74b1fbb452ea70518da1af96d2b54
vendhan0500 <63704813+vendhan0500@users.noreply.github.com>
2023-11-01 22:11:40 +0530

    Delete UI/wloggerUI directory

--- UI/wloggerUI/src/landingpage/RecentBlogs.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './RecentBlogs.css'
-
-export default function RecentBlogs() {
-  return (
-    <div className='recentblogs-container'>RecentBlogs</div>
-  )
-}
--- UI/wloggerUI/src/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import darkModeReducer from './themeToggle/darkModeSlice'
-
-
-export const store = configureStore({
-    reducer:{
-        darkMode: darkModeReducer
-    }
-})
